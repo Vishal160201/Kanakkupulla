ed30915a2d17dfd6ded0b76dc36b17dbdea39678
nithyavishal-r-22218 <nithyavishal.r@zohocorp.com>
2026-06-21 11:06:59 +0530

    fix: hard limit Node memory and enable webVersionCache for WhatsApp

--- whatsapp-bot-server/server.js
+++ whatsapp-bot-server/server.js
@@ -31,6 +31,10 @@ const initWhatsApp = () => {
   
   const client = new Client({
     authStrategy: new LocalAuth({ dataPath: './.wwebjs_auth' }),
+    webVersionCache: {
+      type: 'remote',
+      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    },
     puppeteer: {
       headless: true,
       executablePath: process.env.CHROME_BIN || null,
